ecf631c71ae516e1ae2ca413b9f605443575dcb6
Ludmila A. Gonzalez <ludmigon312@gmail.com>
2023-11-22 10:17:00 -0300

    Balanceador de carga con Haproxy}

---
+++ web2/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Web 2 cargada.")
+})
+
+app.listen(80, ()=>{
+    console.log("Servidor corriendo en http://localhost:80")
+})
